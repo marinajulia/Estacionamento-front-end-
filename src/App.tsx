73dda495a9b554008e92ea3508a4e6e79e8acb3c
marinajulia <marinajulia707@gmail.com>
2022-07-05 08:39:12 -0300

    chore: Initialize header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
 function App() {
   return (
     <div>
-      <p>Teste</p>
+      <p>teste</p>
+      <Header></Header>
+      <GlobalStyle/>
     </div>
   )
 }
